d7b717510291b546dcfdde3259eacd67cfb0473a
吴翱翔 <pylint@yandex.com>
2020-01-06 18:12:33 +0800

    去掉了MainActivity的ActionBar相关代码

--- app/src/main/java/com/monitor/exchange/MainActivity.java
+++ app/src/main/java/com/monitor/exchange/MainActivity.java
@@ -3,7 +3,6 @@ package com.monitor.exchange;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
@@ -19,9 +18,6 @@ public class MainActivity extends AppCompatActivity {
 
     /* 初始化底部导航栏 */
     BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation_menu);
-    // menu.xml中的id必须与navigation_graph.xml中的ID保持一致
-    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home,
-      R.id.navigation_monitor, R.id.navigation_currencies, R.id.navigation_mine).build();
     NavController navController = Navigation.findNavController(this, R.id.navigation_host_fragment);
     NavigationUI.setupWithNavController(bottomNavigation, navController);
   }
