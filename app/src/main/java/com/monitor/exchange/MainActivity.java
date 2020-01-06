0134e17d93b3a40a1d4891a07ee24b5e640b6d14
吴翱翔 <pylint@yandex.com>
2020-01-06 18:36:33 +0800

    照着文档实现了第二种webview实现方法(无需layout)

--- app/src/main/java/com/monitor/exchange/MainActivity.java
+++ app/src/main/java/com/monitor/exchange/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,12 +15,15 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+
+    WebView myWebView = new WebView(getApplicationContext());
+    setContentView(myWebView);
+    myWebView.loadUrl("https://www.raspberrypi.org/");
 
     /* 初始化底部导航栏 */
-    BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation_menu);
-    NavController navController = Navigation.findNavController(this, R.id.navigation_host_fragment);
-    NavigationUI.setupWithNavController(bottomNavigation, navController);
+//    BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation_menu);
+//    NavController navController = Navigation.findNavController(this, R.id.navigation_host_fragment);
+//    NavigationUI.setupWithNavController(bottomNavigation, navController);
   }
 
 }
